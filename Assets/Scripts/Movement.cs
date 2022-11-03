0144d5b168163b3366629f75932e9369b41dc593
VladySlave-P <vlad.polishuk.007@gmail.com>
2022-11-03 21:39:51 +0200

    I love KPI (no)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed = 5f;
+    public float jumpPower = 20f;
+    private Rigidbody2D rb;
+    public bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");// -1 to 1
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("123123");
+            isGrounded = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            Debug.Log("smth");
+            isGrounded = true;
+        }
+    }
+}
